a0c5d444cf1948d363859e497dc6554a787e610b
Merch7x <aquamaingi@gmail.com>
2024-09-05 08:18:53 +0300

    Basic Dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Create a basic cachig mechanism"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Implements a simple caching system"""
+
+    def __init__(self):
+        """Initialize an object"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
