03a940320e9d3d36f112f6fc6f16d615f9670b8d
Merch7x <aquamaingi@gmail.com>
2024-09-05 11:23:12 +0300

    Use order dict to create LRU caching'

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Implemenys LRU caching"""
 from base_caching import BaseCaching
-from functools import lru_cache
 
 
 class LRUCache(BaseCaching):
@@ -13,16 +12,15 @@ class LRUCache(BaseCaching):
         """Initialize an object"""
         super().__init__()
 
-    # @lru_cache(maxsize=BaseCaching.MAX_ITEMS)
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key is None or item is None:
             return
-
-        while len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            k = next(iter(self.cache_data))
-            self.cache_data.pop(k)
+        elif key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            k, v = self.cache_data.popitem(last=False)
             print(f'DISCARD: {k}')
 
         self.cache_data[key] = item
@@ -30,6 +28,8 @@ class LRUCache(BaseCaching):
     def get(self, key):
         """ Get an item by key
         """
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
+        else:
+            self.cache_data.move_to_end(key)
         return self.cache_data.get(key)
