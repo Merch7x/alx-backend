d629236cfcad8d8a07db929503a7e94d5cffa7c0
Merch7x <aquamaingi@gmail.com>
2024-09-10 17:07:57 +0300

    Redo function

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,7 +2,6 @@
 """Create an instance of a flask app"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
-import babel
 
 
 app = Flask(__name__)
@@ -24,14 +23,14 @@ babel = Babel(app)
 def get_locale():
     """Selects a locale for babel"""
     locale = request.args.get('locale')
-    if locale:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(
         app.config['LANGUAGES']
-    )
+    ) or app.config['BABEL_DEFAULT_LOCALE']
 
 
-@app.route('/', methods=['GET'], srtict_slashes=False)
+@app.route('/')
 def index():
     """Define an index route"""
     return render_template("4-index.html")
