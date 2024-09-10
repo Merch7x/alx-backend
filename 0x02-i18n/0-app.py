d2e473117426c5342f8825f9bd86513bb9c84850
Merch7x <aquamaingi@gmail.com>
2024-09-10 12:38:46 +0300

    Instanciate a basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Create an instance of a flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  """Define an index route"""
+  return render_template("0-index.html")
