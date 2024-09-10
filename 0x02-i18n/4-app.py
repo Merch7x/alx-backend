30cd09b939042332f34732169bf54694eaeebbab
Merch7x <aquamaingi@gmail.com>
2024-09-10 17:06:09 +0300

    Update function

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,6 +2,9 @@
 """Create an instance of a flask app"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
+import babel
+
+
 app = Flask(__name__)
 
 
@@ -21,14 +24,14 @@ babel = Babel(app)
 def get_locale():
     """Selects a locale for babel"""
     locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+    if locale:
         return locale
     return request.accept_languages.best_match(
         app.config['LANGUAGES']
-    ) or app.config['BABEL_DEFAULT_LOCALE']
+    )
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], srtict_slashes=False)
 def index():
     """Define an index route"""
     return render_template("4-index.html")
