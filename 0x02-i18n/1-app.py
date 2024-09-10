384d01351b28f2338bf61055c34e3409389264ae
Merch7x <aquamaingi@gmail.com>
2024-09-10 13:35:55 +0300

    Update template to be rendered

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,10 +14,11 @@ class Config:
 app.config.from_object(Config)
 app.config['BABEL_DEFAULT_LOCALE'] = app.config['LANGUAGES'][0]
 app.config['BABEL_DEFAULT_TIMEZONE'] = app.config['TIMEZONE']
-babel = Babel(app)
+babel = Babel(app, default_locale=app.config['LANGUAGES'][0],
+              default_timezone=app.config['TIMEZONE'])
 
 
 @app.route('/')
 def index():
     """Define an index route"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
