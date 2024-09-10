7da116ae76ca1d2051e469cc7923d2a38b6d6405
Merch7x <aquamaingi@gmail.com>
2024-09-10 16:07:43 +0300

    Specify locale in args

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Create an instance of a flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    """Create a configuration class"""
+    LANGUAGES = ["en", "fr"]
+    TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Selects a locale for babel"""
+    locale = request.args.get('locale')
+    if locale or locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES']
+    )
+
+
+@app.route('/')
+def index():
+    """Define an index route"""
+    return render_template("2-index.html")
